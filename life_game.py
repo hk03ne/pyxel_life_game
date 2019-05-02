838e10c72231c797c4a24dbef02ced6b0d7172b4
Hiroya Komine <hk0mine@outlook.jp>
2019-05-02 18:58:04 +0900

    change the size of world to csv size

--- life_game.py
+++ life_game.py
@@ -7,13 +7,14 @@ APP_HEIGHT = 120
 class App:
   def __init__(self):
     self.world = pandas.read_csv("world.csv").values.tolist()
-    pyxel.init(APP_WIDTH, APP_HEIGHT, caption="Life Game")
+    #pyxel.init(APP_WIDTH, APP_HEIGHT, caption="Life Game", fps = 1)
+    pyxel.init(len(self.world[0]), len(self.world), caption="Life Game", fps = 1)
     pyxel.run(self.update, self.draw)
 
   def update(self):
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
-    # self.update_world()
+    #self.update_world()
 
   def draw(self):
     pyxel.cls(0)
@@ -33,29 +34,29 @@ class App:
         count = 0
         if self.world[y-1][x-1]:
           count = count + 1
-        if self.world[y][x-1]:
-          count = count + 1
-        if self.world[y+1][x-1]:
-          count = count + 1
         if self.world[y-1][x]:
-          count = count + 1
-        if self.world[y+1][x]:
           count = count + 1
         if self.world[y-1][x+1]:
           count = count + 1
+        if self.world[y][x-1]:
+          count = count + 1
         if self.world[y][x+1]:
+          count = count + 1
+        if self.world[y+1][x-1]:
+          count = count + 1
+        if self.world[y+1][x]:
           count = count + 1
         if self.world[y+1][x+1]:
           count = count + 1
 
         # セルの生死を判定する
-        if not cell and count == 3:
+        if cell == 0 and count == 3:
           self.world[y][x] = 1
-        elif cell and count == 2 or count == 3:
-          self.world[y][x] = 1
-        elif cell and count <= 1:
+        elif cell and count < 2:
           self.world[y][x] = 0
-        elif cell and count >= 4:
+        elif cell and (count == 2 or count == 3):
+          self.world[y][x] = 1
+        elif cell and 3 < count:
           self.world[y][x] = 0
 
   # 世界を描画する
@@ -63,6 +64,7 @@ class App:
     for y, row in enumerate(self.world):
       for x, cell in enumerate(row):
         if cell:
-          pyxel.pix(x, y + 30, 3)
+          pyxel.pix(x, y, 3)
+    pyxel.text(0, APP_HEIGHT-5, str(pyxel.frame_count),1)
 
 App()
