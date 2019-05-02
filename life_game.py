5b47093cc1cbd0153849178f427923720510a58c
Hiroya Komine <hk0mine@outlook.jp>
2019-05-02 17:58:40 +0900

    add import csv function

--- life_game.py
+++ life_game.py
@@ -1,27 +1,62 @@
 import pyxel
+import pandas
+
 APP_WIDTH = 150
 APP_HEIGHT = 120
 
 class App:
   def __init__(self):
-    self.world = [[1, 0, 1, 0, 0, 1],
-                  [0, 0, 0, 0, 1, 0],
-                  [1, 0, 1, 1, 0, 0]]
+    self.world = pandas.read_csv("world.csv").values.tolist()
     pyxel.init(APP_WIDTH, APP_HEIGHT, caption="Life Game")
     pyxel.run(self.update, self.draw)
 
   def update(self):
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
-    self.update_world()
+    # self.update_world()
 
   def draw(self):
     pyxel.cls(0)
     self.draw_world()
   
-  # 各セルの生死を判断して世界を更新する
+  # 世界を更新する
   def update_world(self):
-    pass
+    for y, row in enumerate(self.world):
+      if y == 0 or y == len(self.world) - 1:
+        continue
+
+      for x, cell in enumerate(row):
+        if x == 0 or x == len(row) - 1:
+          continue
+
+        # 隣接する生きているセルを数える
+        count = 0
+        if self.world[y-1][x-1]:
+          count = count + 1
+        if self.world[y][x-1]:
+          count = count + 1
+        if self.world[y+1][x-1]:
+          count = count + 1
+        if self.world[y-1][x]:
+          count = count + 1
+        if self.world[y+1][x]:
+          count = count + 1
+        if self.world[y-1][x+1]:
+          count = count + 1
+        if self.world[y][x+1]:
+          count = count + 1
+        if self.world[y+1][x+1]:
+          count = count + 1
+
+        # セルの生死を判定する
+        if not cell and count == 3:
+          self.world[y][x] = 1
+        elif cell and count == 2 or count == 3:
+          self.world[y][x] = 1
+        elif cell and count <= 1:
+          self.world[y][x] = 0
+        elif cell and count >= 4:
+          self.world[y][x] = 0
 
   # 世界を描画する
   def draw_world(self):
