e3bf360337421f358331f1f79c04868bf0925f33
Hiroya Komine <hk0mine@outlook.jp>
2019-05-02 16:33:37 +0900

    implement draw_world function

--- life_game.py
+++ life_game.py
@@ -1,17 +1,33 @@
 import pyxel
+APP_WIDTH = 150
+APP_HEIGHT = 120
 
 class App:
-    def __init__(self):
-        pyxel.init(150, 120, caption="Life Game")
-        pyxel.run(self.update, self.draw)
+  def __init__(self):
+    self.world = [[1, 0, 1, 0, 0, 1],
+                  [0, 0, 0, 0, 1, 0],
+                  [1, 0, 1, 1, 0, 0]]
+    pyxel.init(APP_WIDTH, APP_HEIGHT, caption="Life Game")
+    pyxel.run(self.update, self.draw)
 
-    def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+  def update(self):
+    if pyxel.btnp(pyxel.KEY_Q):
+      pyxel.quit()
+    self.update_world()
 
-    def draw(self):
-        pyxel.cls(0)
-        pyxel.text(1, 1, "frame_count: " + str(pyxel.frame_count), 1)
-        pyxel.circ(30, pyxel.frame_count % 100, 5, 2)
+  def draw(self):
+    pyxel.cls(0)
+    self.draw_world()
+  
+  # 各セルの生死を判断して世界を更新する
+  def update_world(self):
+    pass
+
+  # 世界を描画する
+  def draw_world(self):
+    for y, row in enumerate(self.world):
+      for x, cell in enumerate(row):
+        if cell:
+          pyxel.pix(x, y + 30, 3)
 
 App()
